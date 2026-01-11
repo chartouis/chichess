657fe6ab16e766cbbb07efd3d4478dc0e63f3055
Chitos <erna95er@mail.ru>
2026-01-11 23:30:04 +0500

    fix : enable scheduling

--- src/main/java/kz/chitas/chess/ChessApplication.java
+++ src/main/java/kz/chitas/chess/ChessApplication.java
@@ -2,8 +2,12 @@ package kz.chitas.chess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaAuditing
 public class ChessApplication {
 
 	public static void main(String[] args) {
